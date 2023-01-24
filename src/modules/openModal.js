8f5a0714afff7826fbeb552e742199bc0efc7aa8
Mur <murmavilink@gmail.com>
2023-01-24 22:49:03 +0600

    wrote a line check function, added styles to css for title and year

--- src/modules/openModal.js
+++ src/modules/openModal.js
@@ -23,6 +23,16 @@ export const openModal = async ({id, movieDetails, modal}) => {
         }
     });
 
+    // Проверка описание фильма
+    const stringValidation = (str, num) => {
+        if(screen.width <= 620) {
+            return str.trim().length > num ? str.substring(0, num).trim() + '...': str;
+        } else {
+            return str;
+        }
+    };
+
+
     document.body.classList.add('stop-scrolling');
 
     modal.classList.add('modal--show');
@@ -39,9 +49,9 @@ export const openModal = async ({id, movieDetails, modal}) => {
                     <div class="loader"></div>
                     <li class="modal__movie-genre">Жанр - ${respData.genres.map(genreItem => genreItem.genre).join(', ')}</li>
                     ${respData.filmLength ? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>` : ''}
-                    <li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
-                    <li class="modal__movie-overview">Описание - ${respData.description}</li>
-                    ${Object.keys(infoObj).length ? `<li>${infoObj.name} <a class="modal__movie-site" target="_blank" href="${infoObj.url}">${infoObj.url}</a></li>` : ''}
+                    <li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${stringValidation(respData.webUrl, 20)}</a></li>
+                    <li class="modal__movie-overview">Описание - ${stringValidation(respData.description, 200)}</li>
+                    ${Object.keys(infoObj).length ? `<li>${infoObj.name} <a class="modal__movie-site" target="_blank" href="${infoObj.url}">${stringValidation(infoObj.url, 20)}</a></li>` : ''}
                 </ul>
                 <button type="button" class="modal__button-close">Закрыть</button>
             </div>`);
